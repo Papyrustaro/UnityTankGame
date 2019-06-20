8638f9b8adc3c295fc270faec51e9a9f819ab739
SantaroHanafusa <undertale1104@gmail.com>
2019-06-20 11:42:13 +0900

    able menu select by key

--- Assets/Scripts/MenuSceneManager.cs
+++ Assets/Scripts/MenuSceneManager.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuSceneManager : MonoBehaviour
 {
+    public Button OnePlayButton;
+    public Button EveryonePlayButton;
+    public Button UserDataButton;
+    public Button OptionButton;
+
+    private void Start()
+    {
+        OnePlayButton.Select();
+    }
     public void OnOnePlayButtonClicked()
     {
         SceneManager.LoadScene("Main");
