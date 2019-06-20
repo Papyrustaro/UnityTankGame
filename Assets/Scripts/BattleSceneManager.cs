67108edc25ade2bb19d9234f59e00e1cb6571fbb
SantaroHanafusa <undertale1104@gmail.com>
2019-06-21 01:46:01 +0900

    make GameClearScene

--- Assets/Scripts/BattleSceneManager.cs
+++ Assets/Scripts/BattleSceneManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BattleSceneManager : MonoBehaviour
 {
@@ -13,6 +14,14 @@ public class BattleSceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            Invoke("GameClear", 0.5f);
+        }
+    }
+
+    private void GameClear()
+    {
+        SceneManager.LoadScene("GameClear");
     }
 }
---
+++ Assets/Scripts/GameClearSceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameClearSceneManager : MonoBehaviour
+{
+    public Button continueButton;
+    public Button moveMenuSceneButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        continueButton.Select();
+    }
+
+    // Update is called once per frame
+    public void OnContinueButtonClicked()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void OnMoveMenuSceneButtonClicked()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
