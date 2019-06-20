ca2b1d483b122f7bf65ad067943934ed8fda2dce
SantaroHanafusa <undertale1104@gmail.com>
2019-06-20 11:10:10 +0900

    make Enemy3

--- Assets/Scripts/EnemyShotShell.cs
+++ Assets/Scripts/EnemyShotShell.cs
@@ -6,12 +6,13 @@ public class EnemyShotShell : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public float shotSpeed;
-    private float shotIntarval;
+    public float shotIntarval = 60;
+    private float IntarvalCount = 0;
 
     void Update()
     {
-        shotIntarval++;
-        if(shotIntarval % 60 == 0)
+        IntarvalCount++;
+        if(IntarvalCount % shotIntarval == 0)
         {
             GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
