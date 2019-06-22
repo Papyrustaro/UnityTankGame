c76345e8943b44a56df1fc8fae09221eeb16e3fc
SantaroHanafusa <undertale1104@gmail.com>
2019-06-23 00:38:15 +0900

    make pauseMenu

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseUI;
+    public Button resumeButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Ctrl"))
+        {
+            pauseUI.SetActive(!pauseUI.activeSelf);
+            if (pauseUI.activeSelf)
+            {
+                resumeButton.Select();
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+    }
+    public void OnResumeButtonClicked()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void OnRestartButtonClicked()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main");
+    }
+    public void OnGoToMenuButtonClicked()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
