311095896da3b074c6f7bc41893629c13720ceb9
SantaroHanafusa <undertale1104@gmail.com>
2019-07-04 18:09:41 +0900

    make moveTankFloor

---
+++ Assets/Scripts/PutObjectManager/MoveTankFloor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTankFloor : MonoBehaviour
+{
+    public float forceX;
+    public float forceZ;
+    private void FixedUpdate()
+    {
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.MovePosition(rb.position + new Vector3(forceX * Time.deltaTime, 0f, forceZ * Time.deltaTime));
+        }
+    }
+}
