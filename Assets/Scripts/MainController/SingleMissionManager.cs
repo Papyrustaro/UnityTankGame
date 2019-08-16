2a378788b3db1694f6b99339e6ccaad3e975356a
SantaroHanafusa <undertale1104@gmail.com>
2019-08-16 17:07:34 +0900

    create resultScene of singleMission

--- Assets/Scripts/MainController/SingleMissionManager.cs
+++ Assets/Scripts/MainController/SingleMissionManager.cs
@@ -13,9 +13,13 @@ public class SingleMissionManager : MonoBehaviour
     public GameObject missionTitlePanel;
     public GameObject missionClearFlagPanel;
     public GameObject missionFailFlagPanel;
+    public GameObject resultUIPanel;
+    public GameObject scoreLabel;
     private Text missionNumberText;
     private Text enemyCounterText;
     private Text playerLifeText;
+
+    public int allMissionNum = 3;
 
 
     private void Awake()
@@ -44,6 +48,11 @@ public class SingleMissionManager : MonoBehaviour
         {
             Time.timeScale = 1f;
         }));
+    }
+
+    private void Update()
+    {
+        SingleMissionStaticData.countTime += Time.deltaTime;
     }
 
     public void DisplayMissionTitle()
@@ -93,14 +102,24 @@ public class SingleMissionManager : MonoBehaviour
         Time.timeScale = 0f;
         SingleMissionStaticData.missionNumber++;
         SingleMissionStaticData.loadNewStage = true;
-        StartCoroutine(DelayMethod(1f, () =>
+        if(allMissionNum > SingleMissionStaticData.missionNumber)
         {
-            DisplayMissionClearFlag();
-        }));
-        StartCoroutine(DelayMethod(3f, () =>
+            StartCoroutine(DelayMethod(1f, () =>
+            {
+                DisplayMissionClearFlag();
+            }));
+            StartCoroutine(DelayMethod(3f, () =>
+            {
+                SceneManager.LoadScene("SingleMission" + SingleMissionStaticData.missionNumber);
+            }));
+        }
+        else
         {
-            SceneManager.LoadScene("SingleMission" + SingleMissionStaticData.missionNumber);
-        }));
+            StartCoroutine(DelayMethod(1f, () =>
+            {
+                DisplayResult();
+            }));
+        }
     }
 
     //次のミッションに移動したときの処理
@@ -114,13 +133,76 @@ public class SingleMissionManager : MonoBehaviour
     {
         Time.timeScale = 0f;
         SingleMissionStaticData.playerLife--;
-        StartCoroutine(DelayMethod(1f, () =>
+        if(SingleMissionStaticData.playerLife > 0)
         {
-            DisplayMissionFailFlag();
+            StartCoroutine(DelayMethod(1f, () =>
+            {
+                DisplayMissionFailFlag();
+            }));
+            StartCoroutine(DelayMethod(3f, () =>
+            {
+                SceneManager.LoadScene("SingleMission" + SingleMissionStaticData.missionNumber);
+            }));
+        }
+        else
+        {
+            StartCoroutine(DelayMethod(1f, () =>
+            {
+                DisplayResult();
+            }));
+        }
+    }
+
+    public void DisplayResult()
+    {
+        resultUIPanel.SetActive(true);
+        scoreLabel.SetActive(false);
+
+        GameObject titleText = resultUIPanel.transform.Find("TitleText").gameObject;
+        GameObject destroyScoreText = resultUIPanel.transform.Find("DestroyScoreText").gameObject;
+        GameObject lifeScoreText = resultUIPanel.transform.Find("LifeScoreText").gameObject;
+        GameObject timeScoreText = resultUIPanel.transform.Find("TimeScoreText").gameObject;
+        GameObject sumScoreText = resultUIPanel.transform.Find("SumScoreText").gameObject;
+        GameObject sumScoreValueText = resultUIPanel.transform.Find("SumScoreValueText").gameObject;
+        GameObject lineText = resultUIPanel.transform.Find("Line").gameObject;
+
+        int timeScore = 600 - (int)SingleMissionStaticData.countTime;
+        int minute = (int)(SingleMissionStaticData.countTime / 60);
+        int second = (int)SingleMissionStaticData.countTime - minute;
+
+        destroyScoreText.GetComponent<Text>().text = "撃墜点: " + SingleMissionStaticData.playerScore.ToString();
+        lifeScoreText.GetComponent<Text>().text = "残機数: " + SingleMissionStaticData.playerLife * 100 + " (" + SingleMissionStaticData.playerLife + ")";
+        timeScoreText.GetComponent<Text>().text = "かかった時間: " + timeScore + " (" + minute + "分" + second + "秒)";
+
+        sumScoreValueText.GetComponent<Text>().text = (SingleMissionStaticData.playerScore + (SingleMissionStaticData.playerLife * 100) + timeScore).ToString();
+
+        StartCoroutine(DelayMethod(2f, () =>
+        {
+            titleText.SetActive(false);
         }));
         StartCoroutine(DelayMethod(3f, () =>
         {
-            SceneManager.LoadScene("SingleMission" + SingleMissionStaticData.missionNumber);
+            destroyScoreText.SetActive(true);
+        }));
+        StartCoroutine(DelayMethod(4f, () =>
+        {
+            lifeScoreText.SetActive(true);
+        }));
+        StartCoroutine(DelayMethod(5f, () =>
+        {
+            timeScoreText.SetActive(true);
+        }));
+        StartCoroutine(DelayMethod(6f, () =>
+        {
+            lineText.SetActive(true);
+        }));
+        StartCoroutine(DelayMethod(7f, () =>
+        {
+            sumScoreText.SetActive(true);
+        }));
+        StartCoroutine(DelayMethod(8f, () =>
+        {
+            sumScoreValueText.SetActive(true);
         }));
     }
 
--- Assets/Scripts/SingleMission/SingleMissionStaticData.cs
+++ Assets/Scripts/SingleMission/SingleMissionStaticData.cs
@@ -7,9 +7,10 @@ public class SingleMissionStaticData : MonoBehaviour
     public static int selectTankNumber;
     public static int playerScore = 0;
     public static int playerLife = 3;
+    public static float countTime = 0f;
     public static int enemyNumSetStage = 3;
     public static bool[] enemyIsDeath;
     public static int remainEnemyNum = 3;
-    public static int missionNumber = 2;
+    public static int missionNumber = 0;
     public static bool loadNewStage = true;
 }
