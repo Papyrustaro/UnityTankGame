01d9e83214fca57ddd12be5036316b9744b9b4ce
SantaroHanafusa <undertale1104@gmail.com>
2019-11-05 13:27:07 +0900

    survivalモードにおいて、500点まではスキルを持たない敵。1000点から500点獲得するごとに湧き間隔を1秒縮めるように(最小5秒)

--- Assets/Scripts/BattleManager/GenerateEnemyRandom.cs
+++ Assets/Scripts/BattleManager/GenerateEnemyRandom.cs
@@ -8,7 +8,8 @@ public class GenerateEnemyRandom : MonoBehaviour
     public int enemyPrefabKindNum;
     public GameObject[] enemyPrefab;
     private GameObject playerPrefab;
-    public float generateTime;
+    public float generateTime = 10f;
+    private bool isHighScore = false; //500点以上で強い敵も出現
     private float countTime = 0f;
 
     private int[] generatePosition = { -20, 0, 20 };
@@ -27,21 +28,58 @@ public class GenerateEnemyRandom : MonoBehaviour
         countTime += Time.deltaTime;
         if(countTime >= generateTime)
         {
+            CheckHighScore();
             GenerateEnemy();
+            SetGenerateTime();
+
             countTime = 0f;
         }
     }
     public void GenerateEnemy()
     {
-        int enemyNum = UnityEngine.Random.Range(0, enemyPrefabKindNum);
         while (true)
         {
             int x = generatePosition[UnityEngine.Random.Range(0, 3)];
             int z = generatePosition[UnityEngine.Random.Range(0, 3)];
             if (checkGeneratePosition(x, z)){
-                Instantiate(enemyPrefab[enemyNum], new Vector3(x, 0f, z), Quaternion.identity);
-                break;
+                //500点以上の場合は全ての敵からランダム
+                if (this.isHighScore)
+                {
+                    Instantiate(enemyPrefab[UnityEngine.Random.Range(0, enemyPrefabKindNum)], new Vector3(x, 0f, z), Quaternion.identity);
+                    break;
+                }
+                //500点未満の場合はスキルがない敵(0～6)
+                else
+                {
+                    Instantiate(enemyPrefab[UnityEngine.Random.Range(0, 7)], new Vector3(x, 0f, z), Quaternion.identity);
+                    break;
+                }
             }
+        }
+    }
+
+    //1000点から500点獲得するごとにgenerateTimeを1秒短くする。(最小5秒)
+    public void SetGenerateTime()
+    {
+        int minusTime = ((SingleSurvivalStaticData.playerScore - 1000) / 500);
+        if(minusTime <= 0)
+        {
+            return;
+        }else if(minusTime > 5)
+        {
+            this.generateTime = 5f;
+        }
+        else
+        {
+            this.generateTime = 10f - (float)minusTime;
+        }
+    }
+
+    public void CheckHighScore()
+    {
+        if(!this.isHighScore && SingleSurvivalStaticData.playerScore >= 500)
+        {
+            this.isHighScore = true;
         }
     }
 
